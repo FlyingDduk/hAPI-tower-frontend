764734affd3d48eeea35716302ea4e932a86cd73
s248h176 <s248h176@gmail.com>
2021-12-16 12:49:01 -0600

    Start of CollAAAB

--- frontend/src/components/Gameboard/Gameboard.jsx
+++ frontend/src/components/Gameboard/Gameboard.jsx
@@ -4,14 +4,19 @@ import Cell from "../Cell/Cell.jsx";
 
 function Gameboard(props) {
   const grid = [];
- 
-    for (let row = 0; row < 10; row++) {
-      grid.push([]);
-      for (let col = 0; col < 10; col++) {
-        grid[row].push(<Cell key={`${col}${row}`} color="red" />);
-      }
+
+  for (let row = 0; row < 20; row++) {
+    grid.push([]);
+    for (let col = 0; col < 20; col++) {
+      grid[row].push(<Cell key={`${col}${row}`}/>);
+      
     }
-  return grid
+  }
+  return (
+      <div className = "game-board">
+          {grid}
+      </div>
+  )
 }
 
 export default Gameboard;

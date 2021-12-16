7061661248b358dc766d6ec145cdfe5de4d98a9d
s248h176 <s248h176@gmail.com>
2021-12-16 16:38:16 -0600

    Sprites,Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Gameboard from "./components/Gameboard/Gameboard.jsx";
+import Towers from "./components/Towers/Towers.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Tower Boiz</h1>
+        <h1>Halo Towers</h1>
         <nav>
           <ul>
             <li>Rules</li>
@@ -16,13 +17,25 @@ function App() {
       </header>
       <main>
         <Gameboard />
+        <div className="tower-choice">
+          <Towers />
+        </div>
       </main>
       <footer>
         The Tower Boiz
-        <p>Chance Broderick</p>
-        <p>https://www.linkedin.com/in/kinglogan3/</p>
-        <p>https://www.linkedin.com/in/abdullah-ga/</p>
-        <p>https://www.linkedin.com/in/hongssam9/</p>
+        <ul className="navbar">
+        <li><a href = "#">Chance</a></li> 
+          <li>
+            <a href="https://www.linkedin.com/in/kinglogan3">King</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/abdullah-ga/">Abdullah</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/hongssam9/">Samuel</a>
+          </li>
+          
+        </ul>
       </footer>
     </div>
   );

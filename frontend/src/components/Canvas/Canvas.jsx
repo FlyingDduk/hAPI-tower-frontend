767a40ffa6719b17714ab4f63e6c96049fe78b37
s248h176 <s248h176@gmail.com>
2021-12-18 10:10:12 -0600

    Canvas Implementation

--- frontend/src/components/Canvas/Canvas.jsx
+++ frontend/src/components/Canvas/Canvas.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import "./Canvas.css";
 
@@ -15,13 +16,44 @@ function Canvas(props) {
     useEffect(() => {
         const canvas = document.getElementById("canvas1");
         const ctx = canvas.getContext("2d");
+=======
+import React from 'react';
+import { useState, useEffect } from 'react';
+import "./Canvas.css"
+
+function Canvas(props) {
+    /* const canvas = document.getElementById(`canvas1`);
+    const ctx = canvas.getContext(`2d`); */
+    let canvas;
+   /*  let ctx; */
+    
+     const [ctx, setCtx] = useState();
+    useEffect(() => {
+        console.log('hi');
+        canvas = document.getElementById(`canvas1`);
+        setCtx(canvas.getContext('2d'));
+>>>>>>> Canvas Implementation
         canvas.width = 1400;
         canvas.height = 700;
 
         animate();
     }, []);
+<<<<<<< HEAD
 
     
+=======
+    
+/*     canvas.width = 1400;
+    canvas.heigh = 700;  */
+
+   
+
+    const cellSize = 20;
+    const cellGap = 3;
+    const gameGrid = [];
+
+   
+>>>>>>> Canvas Implementation
 
     class Cell {
         constructor(x,y) {
@@ -31,12 +63,17 @@ function Canvas(props) {
             this.height = cellSize;
         }
         draw() {
+<<<<<<< HEAD
             ctx.strokeStyle = "black";
+=======
+            ctx.strokeStyle = `black`;
+>>>>>>> Canvas Implementation
             ctx.strokeRect(this.x, this.y, this.width, this.height);
         }
     }
 
     function createGrid() {
+<<<<<<< HEAD
         for(let y = cellSize; y < canvas.height; y += cellSize) {
             for(let x = 0; x < canvas.width; x += cellSize) {
                 gameGrid.push(new Cell(x,y));
@@ -55,6 +92,30 @@ function Canvas(props) {
         requestAnimationFrame(animate);
     }
 
+=======
+        for(let y = cellSize; y < 1400; y += cellSize){
+            for(let x = 0; x < 700; x += cellSize){
+                gameGrid.push(new Cell((x,y)))
+            }
+        }
+    }
+    createGrid()
+    
+
+    function handleGameGrid(){
+        for(let i = 0; gameGrid.length; i++){
+            gameGrid[i].draw()
+        }
+    }
+    console.log(gameGrid)
+    function animate() {
+      /*   ctx.fillStyle = `blue`;
+        ctx.fillRect(0,0,controlsBar.width, controlsBar.height); */
+        handleGameGrid();
+        requestAnimationFrame(animate);
+    }
+    animate(); 
+>>>>>>> Canvas Implementation
     return (
         <div>
             <canvas id="canvas1"></canvas>

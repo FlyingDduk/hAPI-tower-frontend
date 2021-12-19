5f5b660b027803b7f94f5b81315f2ba196fccc6f
s248h176 <s248h176@gmail.com>
2021-12-19 08:54:36 -0600

    Recover Deleted Stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,20 +9,9 @@ import Canvas from "./components/Canvas/Canvas.jsx"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+       
+      </header> */}
       <main>
         <div className = "gameboard">
           <Canvas />
